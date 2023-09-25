643b0f085fa114d317199e146ef6d655254df258
Caio Teles <caiotelescunha2001@gmail.com>
2023-09-25 16:59:11 -0300

    feat: comentei o código do trabalho de fspd

--- Introdução a sistemas paralelos e distribuidos/Exercício de programação 1/main.c
+++ Introdução a sistemas paralelos e distribuidos/Exercício de programação 1/main.c
@@ -3,13 +3,11 @@
 #include <pthread.h>
 #include "spend_time.h"
 
-// pythreads
-
 // tipo abstrato de dados para representar recursos
 typedef struct{
     int livres[8];
 } Resources;
-// global resources
+// ponteiro para a estrutura de recursos
 Resources *resources;
 
 // tipo abstrato de dados para representar threads
@@ -28,39 +26,7 @@ pthread_mutex_t mutex;
 //condição para testar se todos os recursos estão livres
 pthread_cond_t recursos_livres;
 
-
-//void* start_routine(void *arg);
-//int pthread_create(pthread_t *thread, pthread_attr_t *attr,
-//void *(*start_routine)(void *), void *arg);
-//int pthread_join(pthread_t thread, void **value_ptr);
-//pthread_t pthread_self(void);
-//void pthread_exit(void * value_ptr);
-
-
-
-//pthread_mutex_init( &mutex, NULL /* atributos */ );
-//pthread_mutex_lock( &mutex );
-// seção crítica
-//pthread_mutex_unlock( &mutex );
-
-
-
-
-//thread_cond_t condition;
-//pthread_cond_init ( &condition, &attr );
-//pthread_cond_destroy ( &condition);
-//pthread_cond_wait ( &condition, &mutex );
-//pthread_cond_signal ( &condition ); /* sinaliza um */
-
-// 8 recursos
-
-// mecanismo quando chamado deve permitir que 
-// um thread indique quais dos 8 ela precisa
-// e só prossegue quando ela pega todos
-
-// max 1000 threads
-// todas identificadores diferentes
-
+//função para debugar
 void escreve_array(int *array, int size){
     for(int i = 0; i < size; i++){
         printf(" %d", array[i]);
@@ -86,6 +52,7 @@ void init_recursos(void){
         resources->livres[i] = 1;
     }
 }
+
 void trava_recursos(int *recursos){
     for(int i = 0; i < 8; i++){
         if(recursos[i] != -1){
@@ -95,8 +62,6 @@ void trava_recursos(int *recursos){
 }
 
 void libera_recursos(int *recursos){
-// uma thread que já reservou alguns recursos sinaliza que completou seu trabalho crítico,
-// liberando os recursos.
     for(int i = 0; i < 8; i++){
         if(recursos[i] != -1){
             resources->livres[recursos[i]] = 1;
@@ -123,7 +88,11 @@ void *rotina_inicial(void *data){
     trava_recursos(lista_de_recursos);
     pthread_mutex_unlock( &mutex );
     spend_time(tid,"C",tcritico);
+    //liberar recursos pode ser feito de maneira paralela pelas threads
+    //como cada thread aloca recursos diferentes, não há condição de corrida
     libera_recursos(lista_de_recursos);
+    //sinaliza que os recursos estão livres para que as threads esperando
+    //possam testar novamente
     pthread_cond_signal ( &recursos_livres );
     pthread_exit(NULL);
 }
@@ -183,26 +152,3 @@ int main(void){
     pthread_cond_destroy ( &recursos_livres );
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// No início da thread, ela deve receber como parâmetros seu tid, os tempos livre e crítico
-//    e os recursos que ela necessita para completar seu trabalho
-// Em segunda, ela deve executar as operações:
-//   spend_time(tid,NULL,tlivre);
-//   trava_recursos(recursos);     // a forma de representar os recursos é uma decisão do desenvolvedor
-//   spend_time(tid,"C",tcritico);
-//   libera_recursos();            // note que cada thread deve ser ter sua lista de recursos registrada em algum lugar
-//   pthread_exit(); 
